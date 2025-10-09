fde92bbe1431022d2898a997780b8a463c12dee2
Olivia Griffin <oliviagriffindev@gmail.com>
2025-10-09 11:02:35 -0500

    Create monthly-calendar.py

---
+++ python-mini-projects/calendar/monthly-calendar.py
@@ -0,0 +1,6 @@
+import calendar          # loads Python’s calendar module
+
+yy = 2025                # sets the year
+mm = 12                  # sets the month (December)
+
+print(calendar.month(yy, mm))  # prints that month’s calendar in text form

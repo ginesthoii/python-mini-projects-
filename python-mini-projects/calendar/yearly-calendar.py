ba2b139ddaeee44ccbeacabe3e82a2f1aafee33f
Olivia Griffin <oliviagriffindev@gmail.com>
2025-10-09 11:01:51 -0500

    Create yearly-calendar.py

---
+++ python-mini-projects/calendar/yearly-calendar.py
@@ -0,0 +1,6 @@
+import calendar          # loads Python’s calendar module
+
+yy = 2025                # sets the year
+mm = 12                  # sets the month (December)
+
+print(calendar.calendar(2025)) # prints yearly calendar in text form
